e7726d37f10621fefee1eae9cf06894d4f9facc3
shiga <hikikomorim@gmail.com>
2023-07-07 20:04:01 +0900

    Ok

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,9 +1,21 @@
 package koschei.models;
 
+import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Component
 public class Egg6 {
+
+    @Autowired
+    private Needle7 needle;
+
+    @Autowired
+    public Egg6(Needle7 needle) {
+        this.needle = needle;
+    }
 
     @Override
     public String toString() {
-        return ", в яйце иголка " + "";
+        return ", в яйце иголка " + needle.toString();
     }
 }
